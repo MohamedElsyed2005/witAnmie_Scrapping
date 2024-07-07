e8a9834586db9868c5bc4c35654feeaadf8048a5
mohamed <2305273@anu.edu.eg>
2024-07-07 15:11:03 +0300

    new

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import pandas as pd
 
 
 page = requests.get("https://witanime.quest/%d9%85%d9%88%d8%a7%d8%b9%d9%8a%d8%af-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a7%d8%aa/")
@@ -19,14 +20,9 @@ for i in range (number_of_days):
 
     for j in range(number_of_anime):
         names_of_anime = All_anime[j].find('h3').text.strip()
-        n.append({"اليوم":days , "الانمي" : names_of_anime})
+        n.append({days:names_of_anime})
 
 
-keys = n[0].keys()
-
-with open("C:\\Users\\20100\\Documents\\PythonProjects\\Web_Scraping\\animeEp.csv","w") as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader(keys)
-        dict_writer.writerows(n)
-        print("file is created")
+for i  in range(len(n)):
+    print(pd.Series(n[i]))
 
